5fdfb97600dba1e0347d496a665725f22676a640
ExbotsSites <ita.exbot@gmail.com>
2025-10-09 19:56:40 +0200

    Update over.js

--- commands/over.js
+++ commands/over.js
@@ -11,7 +11,7 @@ export default {
 
   async execute(interaction) {
     const note = interaction.options.getString("note") || "No additional notes provided.";
-    const sessionHostRoleId = "1416802953146400840";
+    const sessionHostRoleId = "1400142649289216040";
 
     if (!interaction.member.roles.cache.has(sessionHostRoleId)) {
       return await interaction.reply({
